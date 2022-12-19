86f177249eef4fe3ded5e9ee85ea7b94629eda8e
François Hodierne <francois@hodierne.net>
2022-12-19 17:49:13 +0100

    personal token based authentication

--- server/graphql/common/expenses.ts
+++ server/graphql/common/expenses.ts
@@ -684,7 +684,8 @@ async function validateExpensePayout2FALimit(req, host, expense, expensePaidAmou
     1000000,
   );
 
-  const twoFactorSession = req.jwtPayload?.sessionId || (req.clientApp?.id && `app_${req.clientApp.id}`);
+  const twoFactorSession =
+    req.jwtPayload?.sessionId || (req.personalToken?.id && `personalToken_${req.personalToken.id}`);
 
   const currentPaidExpenseAmountCache = await cache.get(expensePaidAmountKey);
   const currentPaidExpenseAmount = currentPaidExpenseAmountCache || 0;
